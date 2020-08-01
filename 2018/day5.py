e0d085f195b5002650566cacd185c12dabb7d63d
Ben Silverman <bensilverman10@gmail.com>
2020-08-01 11:27:55 +0100

    Day 5 solution

--- 2018/day5.py
+++ 2018/day5.py
@@ -1,18 +1,40 @@
 poly = list(open("day5.txt", "r").read())
 
-reacted = None
-while reacted or reacted is None:
-    reacted = False
-    i = 0
-    while i < len(poly) - 1:
-        if poly[i] == poly[i+1].swapcase():
-            del poly[i]
-            del poly[i]
 
-            reacted = True
-        i += 1
+def react(poly):
+    reacted = None
+    while reacted or reacted is None:
+        reacted = False
+        i = 0
+        while i < len(poly) - 1:
+            if poly[i] == poly[i+1].swapcase():
+                del poly[i]
+                del poly[i]
 
-    if not reacted:
-        break
+                reacted = True
+            i += 1
 
-print(len(poly))
+        if not reacted:
+            break
+
+    return poly
+
+
+# Part 1
+part_1 = react(poly)
+print(f"Part 1: {len(part_1)}")
+
+# Part 2
+units = set()
+for char in poly:
+    units.add(char.lower())
+
+min_length = None
+
+for char in units:
+    removed_poly = [x for x in poly if x not in [char, char.upper()]]
+    reaction = len(react(removed_poly))
+    if min_length is None or reaction < min_length:
+        min_length = reaction
+
+print(f"Part 2: {min_length}")
