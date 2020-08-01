255ff0d15e0f58fe533b1f77cb0905e5246b3d14
Ben Silverman <bensilverman10@gmail.com>
2020-08-01 15:50:04 +0100

    Write Day 6 Part 1 solution

--- 2018/day6/day6.py
+++ 2018/day6/day6.py
@@ -1,5 +1,7 @@
+from collections import defaultdict
+
 data = [list(map(int, i.split(", ")))
-        for i in open("day6.test", "r").readlines()]
+        for i in open("day6.txt", "r").readlines()]
 
 
 def manhattan(a, b):
@@ -11,6 +13,11 @@ max_y = max(list(zip(*data))[1])
 
 grid = {}
 
+"""
+Determine which of the named coordinates each grid square is closest to.
+Produces a dictionary with tuples for keys, and the named coordinates as values.
+Named coordinate is marked as "-1" if it is equidistant between two named coordinates.
+"""
 for x in range(max_x):
     for y in range(max_y):
         min_dist = min(manhattan([x, y], [i, j])for i, j in data)
@@ -21,4 +28,30 @@ for x in range(max_x):
                     break
                 grid[(x, y)] = n
 
-print(grid)
+# print(f"Grid: {grid}")
+
+
+"""
+Determine which of the named coordinates have infinite areas.
+This is done by seeing which named coordinates "have" grid squares lying on the edge of the grid.
+"""
+infinite_points = set()
+
+for k, v in grid.items():
+    if k[0] in [0, max_x] or k[1] in [0, max_y]:
+        infinite_points.add(grid[k])
+
+# print(f"Infinite Points: {infinite_points}")
+
+"""
+Count the number of occurrences of each named coordinate in our grid, ignoring equidistant points
+or coordinates with infinite area.
+"""
+
+counts = defaultdict(int)
+
+for v in grid.values():
+    if v != -1 and v not in infinite_points:
+        counts[v] += 1
+
+print(f"Part 1: {max(counts.values())}")
