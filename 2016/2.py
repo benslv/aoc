9367e219ba79cbe53f815efea5a76374d6a49f38
Ben Silverman <bensilverman10@gmail.com>
2020-11-21 18:52:51 +0000

    Day 2 solution

--- 2016/2.py
+++ 2016/2.py
@@ -2,8 +2,8 @@ with open("2.in", "r") as f:
     instructions = f.read().splitlines()
 
 # Initialised at (1, 1) because we start on button 5.
-x = 1
-y = 1
+x = 0
+y = 2
 
 dX = {"U": 0, "R": 1, "D": 0, "L": -1}
 dY = {"U": -1, "R": 0, "D": 1, "L": 0}
@@ -15,25 +15,20 @@ dY = {"U": -1, "R": 0, "D": 1, "L": 0}
 keypad = [["X", "X", "1", "X", "X"],
           ["X", "2", "3", "4", "X"],
           ["5", "6", "7", "8", "9"],
-          ["X", "X", "2", "X", "X"],
-          ["X", "X", "2", "X", "X"]]
+          ["X", "A", "B", "C", "X"],
+          ["X", "X", "D", "X", "X"]]
 
 code = ""
 
 for instr in instructions:
     for d in instr:
         x += dX[d]
-        y += dY[d]
+        if x not in range(0, 5) or keypad[y][x] == "X":
+            x -= dX[d]
 
-        # Prevent going out of range.
-        if x < 0:
-            x = 0
-        elif x > 2:
-            x = 2
-        elif y < 0:
-            y = 0
-        elif y > 2:
-            y = 2
+        y += dY[d]
+        if y not in range(0, 5) or keypad[y][x] == "X":
+            y -= dY[d]
 
     code += keypad[y][x]
 
