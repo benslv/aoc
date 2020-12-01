736e2040d61cce3711868cc638421c1d89b424c6
Ben Silverman <bensilverman10@gmail.com>
2020-12-01 08:17:23 +0000

    rename

--- 2020/Day 1/1.py
+++ 2020/Day 1/1.py
@@ -1,11 +1,12 @@
 import fileinput
 
 nums = [int(x) for x in fileinput.input()]
+n = len(nums)
 
 part_1 = 0
 
-for i in range(len(nums)):
-    for j in range(i, len(nums)):
+for i in range(n):
+    for j in range(i, n):
         if nums[i]+nums[j] == 2020:
             part_1 = nums[i]*nums[j]
 
@@ -13,9 +14,9 @@ print(f"Part 1: {part_1}")
 
 part_2 = 0
 
-for x in range(len(nums)):
-    for y in range(x+1, len(nums)):
-        for z in range(y + 1, len(nums)):
+for x in range(n):
+    for y in range(x+1, n):
+        for z in range(y + 1, n):
             if nums[x]+nums[y]+nums[z] == 2020:
                 part_2 = nums[x]*nums[y]*nums[z]
 
