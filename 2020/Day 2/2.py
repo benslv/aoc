7f2027bd0a4a7a747cf4a67a6e80b080c4b96d2a
Ben Silverman <bensilverman10@gmail.com>
2020-12-02 07:07:16 +0000

    Clean up code to use XOR

--- 2020/Day 2/2.py
+++ 2020/Day 2/2.py
@@ -5,17 +5,17 @@ passwords = list(fileinput.input())
 
 def get_valid_passwords(passwords):
     valid = 0
-    for password in passwords:
-        parts = password.split()
+    for line in passwords:
+        parts = line.split()
 
         low, high = [int(x) for x in parts[0].split("-")]
 
-        char = parts[1].rstrip(":")
+        char = parts[1][0]
 
         password = parts[2]
 
         if part_2:
-            if (password[low-1] == char or password[high-1] == char) and password[low-1] != password[high-1]:
+            if (password[low-1] == char) ^ (password[high-1] == char):
                 valid += 1
         else:
             if password.count(char) in range(low, high+1):
