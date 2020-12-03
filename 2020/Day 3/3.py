90eab0bd32af18f6b45f23e91e7c47f00eb16906
Ben Silverman <bensilverman10@gmail.com>
2020-12-03 07:47:00 +0000

    Day 3 solution

---
+++ 2020/Day 3/3.py
@@ -0,0 +1,34 @@
+import fileinput
+
+lines = [line.strip() for line in fileinput.input()]
+
+x = 0
+part_1 = 0
+
+for line in lines:
+    part_1 += line[x] == "#"
+    x = (x + 3) % len(line)
+
+print("Part 1:", part_1)
+
+# Right, Down
+slopes = [[1, 1],
+          [3, 1],
+          [5, 1],
+          [7, 1],
+          [1, 2]]
+
+
+# Init at 1 because multiplying onto it.
+part_2 = 1
+
+for slope in slopes:
+    x, y = 0, 0
+    trees = 0
+    while y < len(lines):
+        trees += lines[y][x] == "#"
+        x = (x + slope[0]) % len(lines[y])
+        y = (y + slope[1])
+    part_2 *= trees
+
+print(part_2)
