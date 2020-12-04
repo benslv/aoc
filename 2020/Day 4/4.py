765c3c6d531d9bcfef5f8c83110403088632812b
Ben Silverman <bensilverman10@gmail.com>
2020-12-04 08:01:19 +0000

    Replace range with inequality

--- 2020/Day 4/4.py
+++ 2020/Day 4/4.py
@@ -16,9 +16,9 @@ for passport in lines:
 
 
         valid_fields = [
-            int(fd["byr"]) in range(1920, 2003),                                     # Birth year is between 1920 and 2002 inclusive.
-            int(fd["iyr"]) in range(2010, 2021),                                     # Issue year is between 2010 and 2020 inclusive.
-            int(fd["eyr"]) in range(2020, 2031),                                     # Expiration year is between 2020 and 2030 inclusive.
+            1920 <= int(fd["byr"]) <= 2002,                                     # Birth year is between 1920 and 2002 inclusive.
+            2010 <= int(fd["iyr"]) <= 2020,                                     # Issue year is between 2010 and 2020 inclusive.
+            2020 <= int(fd["eyr"]) <= 2030,                                     # Expiration year is between 2020 and 2030 inclusive.
             (fd["hgt"][-2:] == "cm" and int(fd["hgt"][:-2]) in range(150, 194)       # Height is in cm and between 150 and 193, or...
              ) or (fd["hgt"][-2:] == "in" and int(fd["hgt"][:-2]) in range(59, 77)), # Height is in inches and between 59 and 76.
             re.match(r"^#[0-9a-z]{6}$", fd["hcl"]),                                  # Hair colour is a valid hexadecimal colour.
