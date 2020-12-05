ddb0de3336f141567543cdb3d35c5cdb445bf626
Ben Silverman <bensilverman10@gmail.com>
2020-12-05 08:44:24 +0000

    Write faster method

--- 2020/Day 5/5.py
+++ 2020/Day 5/5.py
@@ -2,52 +2,16 @@ import fileinput
 
 passes = [line.strip() for line in fileinput.input()]
 
-
-def partition_row(letters, lo, hi):
-    print(lo, hi, letters)
-    # If the range is only one apart, we can work out which value it should be.
-    if hi-lo == 1:
-        return lo if letters[0] == "F" else hi
-
-    # Otherwise, calculate the midpoint and return
-    mid = (lo + hi) // 2
-
-    if letters[0] == "F":
-        return partition_row(letters[1:], lo, mid)
-    else:
-        return partition_row(letters[1:], mid+1, hi)
-
-
-def partition_col(letters, lo, hi):
-    print(lo, hi, letters)
-    # If the range is only one apart, we can work out which value it should be.
-    if hi-lo == 1:
-        return lo if letters[0] == "L" else hi
-
-    # Otherwise, calculate the midpoint and return
-    mid = (lo + hi) // 2
-
-    if letters[0] == "L":
-        return partition_col(letters[1:], lo, mid)
-    else:
-        return partition_col(letters[1:], mid+1, hi)
-
-
 ids = []
 
 for pass_ in passes:
-    row = partition_row(pass_[:7], 0, 127)
-    print(f"row: {row}")
+    seat = pass_.replace("F", "0").replace("B", "1").replace("L", "0").replace("R", "1")
 
-    col = partition_col(pass_[7:], 0, 7)
-    print(f"col: {col}")
-
-    ids.append(row*8+col)
+    ids.append(int(seat, 2))
 
 print(f"Part 1: {max(ids)}")
 
 ids.sort()
-
 
 for i in range(len(ids) - 1):
     if ids[i+1] - ids[i] == 2:
