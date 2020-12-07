36cea4e1d142706f97fa65e02d36980be3e08cae
Ben Silverman <bensilverman10@gmail.com>
2020-12-07 10:21:41 +0000

    Solution to Day 7

--- 2020/Day 7/7.py
+++ 2020/Day 7/7.py
@@ -2,20 +2,21 @@ from collections import defaultdict
 import fileinput
 
 lines = [line.split() for line in fileinput.input()]
+# lines = [line.split() for line in open("7.test2").readlines()]
 
 bags = defaultdict(dict)  # Stores which colour bags each bag contains.
 
 # Parse input and construct dictionary of bag colours.
 for line in lines:
     outer = " ".join(line[0:2])
- 
+
     inners = {" ".join(line[4+i: 4+i+2]): line[4+i-1] for i in range(1, len(line[4:]), 4)}
 
     bags[outer] = inners if "other bags." not in inners else {}
 
 
 def search(colour):
-    if not bags[colour].keys():  # If the bag contains no bags inside it.
+    if not bags[colour]:  # If the bag contains no bags inside it.
         return False
     elif any("shiny gold" in colour for colour in bags[colour].keys()):
         return True
@@ -23,6 +24,19 @@ def search(colour):
         return any([search(inner) for inner in bags[colour]])
 
 
+def search_num(colour):
+    print(bags[colour])
+    # If the bag contains no other bags, return one (for the bag itself).
+    if not bags[colour].keys():
+        return 1
+    else:
+        return sum([int(v)*search_num(k) for k, v in bags[colour].items()]) + 1
+
+
 part_1 = sum([search(colour) for colour, _ in bags.items()])
 
 print(part_1)
+
+# Don't want to include the outer bag in the count.
+part_2 = search_num("shiny gold") - 1
+print(part_2)
