a3bcc1128e7218cf79b1ada9a465c5a442a1c68f
Ben Silverman <bensilverman10@gmail.com>
2020-12-11 10:47:30 +0000

    Comment solution

--- 2020/Day 11/11.py
+++ 2020/Day 11/11.py
@@ -1,47 +1,52 @@
-import fileinput
+import sys
 from copy import deepcopy
 
-lines = [list(line.rstrip()) for line in fileinput.input()]
-
-"""
-    If a seat is empty (L) and there are no occupied seats adjacent to it, the seat becomes occupied.
-    If a seat is occupied (#) and four or more seats adjacent to it are also occupied, the seat becomes empty.
-    Otherwise, the seat's state does not change.
-"""
+lines = [list(line.rstrip()) for line in sys.stdin]
 
 
-def get_empty_neighbours(x: int, y: int, seats: list[str]) -> int:
+def get_empty_neighbours(x, y, seats):
+    # Create matrix of relative coordinates to adjacent seats.
     adjacency = [(i, j) for i in (-1, 0, 1)
                  for j in (-1, 0, 1) if not (i == j == 0)]
 
     count = 0
+    # For each adjacent cell...
     for dx, dy in adjacency:
+        # ...check that it's within the boundaries of the seating grid.
         if 0 <= (x + dx) < len(seats[dy]) and 0 <= (y + dy) < len(seats):
+            # ...increment the count if it's occupied.
             count += seats[y+dy][x+dx] == "#"
 
     return count
 
 
 def get_empty_neighbours_2(x, y, seats):
+    # Create matrix of relative coordinates to adjacent seats.
     adjacency = [(i, j) for i in (-1, 0, 1)
                  for j in (-1, 0, 1) if not (i == j == 0)]
 
     count = 0
+    # For each direction to a "neighbour" cell...
     for i, j in adjacency:
+        # ...initially check a distance of 1 away (i.e. literally adjacent)
         distance = 1
         while True:
-            dx = i * distance
-            dy = j * distance
+            # ...calculate the absolute coordinate of the cell.
+            dx = i * distance + x
+            dy = j * distance + y
 
-            if 0 <= (x + dx) < len(seats[y]) and 0 <= (y + dy) < len(seats):
-                if seats[y+dy][x+dx] == "#":
-                    count += 1
-                    break
-                elif seats[y+dy][x+dx] == "L":
-                    break
-            else:
+            # ...check that the cell is within the boundaries of the seating grid.
+            if not(0 <= (dx) < len(seats[y]) and 0 <= (dy) < len(seats)):
                 break
 
+            # ...check whether the cell contains a seat, and whether it's occupied.
+            if seats[dy][dx] == "#":
+                count += 1
+                break
+            elif seats[dy][dx] == "L":
+                break
+
+            # If no seat was found, increase the distance by 1 and check again.
             distance += 1
 
     return count
@@ -50,9 +55,12 @@ def get_empty_neighbours_2(x, y, seats):
 def step(seats, count_function, vos=4):
     next_state = deepcopy(seats)
 
+    # Iterate through all the cells in the grid.
     for y in range(len(seats)):
         for x in range(len(seats[y])):
+            # If the cell contains a seat...
             if seats[y][x] != ".":
+                # ...calculate how many adjacent cells are occupied seats
                 adj = count_function(x, y, seats)
                 if adj == 0:
                     next_state[y][x] = "#"
@@ -65,10 +73,12 @@ def step(seats, count_function, vos=4):
 seats = deepcopy(lines)
 prev_seats = []
 
+# Step through the simulation until there's no change between two iterations.
 while prev_seats != seats:
     prev_seats = seats
     seats = step(seats, get_empty_neighbours)
 
+# Count the number of occupied seats in the final seating grid.
 part_1 = sum([row.count("#") for row in seats])
 print(f"Part 1: {part_1}")
 
@@ -76,9 +86,12 @@ print(f"Part 1: {part_1}")
 seats = deepcopy(lines)
 prev_seats = []
 
+# Step through the simulation until there's no change between two iterations.
 while prev_seats != seats:
     prev_seats = seats
+    # Same as above, but the number of occupied seats required for a 
     seats = step(seats, get_empty_neighbours_2, vos=5)
 
+# Count the number of occupied seats in the final seating grid.
 part_2 = sum([row.count("#") for row in seats])
 print(f"Part 2: {part_2}")
