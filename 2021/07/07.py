04613337bf56258d27d74b4c51576b61f9d4713d
Ben Silverman <bensilverman10@gmail.com>
2021-12-07 16:23:05 +0000

    Space it out a bit

--- 2021/07/07.py
+++ 2021/07/07.py
@@ -8,8 +8,10 @@ def dist(d):
 
 
 part1 = min(sum(abs(val-x) for x in inp) for val in inp)
+
 part2 = min(sum(dist(abs(val-x)) for x in inp)
             for val in range(min(inp), max(inp)+1))
 
 print("Part 1:", int(part1))
+
 print("Part 2:", int(part2))
