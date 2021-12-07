cb6789ffc9d9849c87618c54119a475fcf386ba8
Ben Silverman <bensilverman10@gmail.com>
2021-12-07 16:21:33 +0000

    Day 7 solutions

---
+++ 2021/07/07.py
@@ -0,0 +1,15 @@
+import sys
+
+inp = list(map(int, sys.stdin.read().split(",")))
+
+
+def dist(d):
+    return d*(d+1)/2
+
+
+part1 = min(sum(abs(val-x) for x in inp) for val in inp)
+part2 = min(sum(dist(abs(val-x)) for x in inp)
+            for val in range(min(inp), max(inp)+1))
+
+print("Part 1:", int(part1))
+print("Part 2:", int(part2))
