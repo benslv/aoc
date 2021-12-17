9bdd7d0c22fa1fe64d63bbeb526a923e5085d0d2
Ben Silverman <bensilverman10@gmail.com>
2021-12-17 09:23:59 +0000

    Optimised search range

--- 2021/17/17.py
+++ 2021/17/17.py
@@ -3,46 +3,35 @@
 part_1 = 0
 part_2 = 0
 
-def solve(i, j, k):
-    part_1 = 0
-    part_2 = 0
+for DX in range(284):
+    for DY in range(-111, 107):
+        dx = DX
+        dy = DY
+        max_y = 0
 
-    for DX in range(i):
-        for DY in range(k, j):
-            dx = DX
-            dy = DY
-            max_y = 0
+        x = 0
+        y = 0
 
-            x = 0
-            y = 0
+        for t in range(500):
+            x += dx
+            y += dy
 
-            for t in range(500):
-                x += dx
-                y += dy
+            if dx > 0:
+                dx -= 1
+            elif dx < 0:
+                dx += 1
 
-                if dx > 0:
-                    dx -= 1
-                elif dx < 0:
-                    dx += 1
+            dy -= 1
 
-                dy -= 1
+            max_y = max(max_y, y)
 
-                max_y = max(max_y, y)
+            if 230 <= x <= 283 and -107 <= y <= -57:
+                part_2 += 1
+                part_1 = max(part_1, max_y)
+                break
 
-                if 230 <= x <= 283 and -107 <= y <= -57:
-                    print(DX, DY, max_y)
-                    part_2 += 1
-                    part_1 = max(part_1, max_y)
-                    break
+            if x > 283 or y < -107:
+                break
 
-                if x > 283 or y < -107:
-                    break
-
-    return (part_1, part_2)
-
-
-for x in range(500):
-    for y in range(500):
-        for z in range(-500, 0, 1):
-            if solve(x,y,z) == (5716, 4556):
-                print(x,y,z)
+print("Part 1:", part_1)
+print("Part 2:", part_2)
