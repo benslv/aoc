5fcea4aed91bf4319ee9fb6cef28b3515c05e91a
benslv <bensilverman10@gmail.com>
2022-12-13 11:53:01 +0000

    2022 Day09

--- 2022/09/09.py
+++ 2022/09/09.py
@@ -18,27 +18,66 @@ DX = {
     "R": 1
 }
 
-# [x, y]
-h_pos = [0, 0]
-prev_h_pos = (0, 0)
-t_pos = (0, 0)
 
-t_spaces = set()
-t_spaces.add((0,0))
+def move_tail(head, tail):
+    t_x, t_y = tail[0], tail[1]
 
-for line in lines:
-    direction = line.split(" ")[0]
-    distance = int(line.split(" ")[1])
+    if abs(head[0] - tail[0]) > 1 or abs(head[1] - tail[1]) > 1:
+        if head[0] > tail[0]:
+            t_x += 1
+        elif head[0] < tail[0]:
+            t_x -= 1
 
-    for _ in range(distance):
-        prev_h_pos = tuple(h_pos)
+        if head[1] > tail[1]:
+            t_y += 1
+        elif head[1] < tail[1]:
+            t_y -= 1
 
-        h_pos[0] += DX[direction]
-        h_pos[1] += DY[direction]
+    return (t_x, t_y)
 
-        if abs(h_pos[0] - t_pos[0]) > 1 or abs(h_pos[1] - t_pos[1]) > 1:
-            t_pos = prev_h_pos
+
+def part_1():
+    h_pos = [0, 0]
+    t_pos = (0, 0)
+
+    t_spaces = set()
+    t_spaces.add((0, 0))
+
+    for line in lines:
+        direction, distance = line.split(" ")
+
+        for _ in range(int(distance)):
+            h_pos[0] += DX[direction]
+            h_pos[1] += DY[direction]
+
+            t_pos = move_tail(h_pos, t_pos)
             t_spaces.add(t_pos)
 
-part_1 = len(t_spaces)
-print(f"Part 1: {part_1=}")
+    return len(t_spaces)
+
+
+print(f"Part 1: {part_1()=}")
+
+
+def part_2():
+    knots = [(0, 0) for _ in range(10)]
+    t_spaces = set()
+    t_spaces.add((0, 0))
+
+    for line in lines:
+        direction, distance = line.split(" ")
+
+        for _ in range(int(distance)):
+
+            knots[0] = (knots[0][0] + DX[direction],
+                        knots[0][1] + DY[direction])
+
+            for i in range(1, len(knots)):
+                knots[i] = move_tail(knots[i-1], knots[i])
+
+            t_spaces.add(knots[9])
+
+    return len(t_spaces)
+
+
+print(f"Part 2: {part_2()=}")
