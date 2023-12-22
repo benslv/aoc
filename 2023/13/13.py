f7d93b6f2b987e7636ddaadc65583f3c9358facc
Ben Silverman <bensilverman10@gmail.com>
2023-12-22 22:50:56 +0000

    day 13

--- 2023/13/13.py
+++ 2023/13/13.py
@@ -7,25 +7,22 @@ def transpose(pattern):
     return list(map(list, zip(*pattern)))
 
 
-def find_reflection(pattern):
-    for start in range(len(pattern)):
-        is_reflecting = False
+def find_reflection(pattern, errors_allowed=0):
 
-        for i in range(start-1, -1, -1):
-            j = start + (start - i) - 1
+    for start in range(len(pattern)):
+        comparisons = []
+
+        for i in range(start, -1, -1):
+            j = start + (start - i) + 1
 
             if j > len(pattern) - 1:
                 break
 
-            if pattern[i] == pattern[j]:
-                is_reflecting = True
+            for x in range(len(pattern[i])):
+                comparisons.append(pattern[i][x] != pattern[j][x])
 
-            if pattern[i] != pattern[j]:
-                is_reflecting = False
-                break
-
-        if is_reflecting:
-            return start
+        if len(comparisons) > 0 and sum(comparisons) == errors_allowed:
+            return start + 1
 
     return 0
 
@@ -40,3 +37,15 @@ for pattern in patterns:
 part_1 = num_cols + 100*num_rows
 
 print(f"{part_1=}")
+
+
+num_cols = 0
+num_rows = 0
+
+for pattern in patterns:
+    num_rows += find_reflection(pattern, 1)
+    num_cols += find_reflection(transpose(pattern), 1)
+
+part_2 = num_cols + 100*num_rows
+
+print(f"{part_2=}")
