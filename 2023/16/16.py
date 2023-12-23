8931991e57e900fad95dbe32f6af7263c5db638a
Ben Silverman <bensilverman10@gmail.com>
2023-12-23 10:56:04 +0000

    2023 Day 16 Part 2

--- 2023/16/16.py
+++ 2023/16/16.py
@@ -3,9 +3,6 @@ from collections import namedtuple
 
 inp = sys.stdin.read().splitlines()
 
-# inp = open(
-#     "/Users/ben.silverman/Development/aoc/2023/16/16.test").read().splitlines()
-
 grid = {
     (y, x): inp[y][x] for y in range(len(inp)) for x in range(len(inp[y]))
 }
@@ -19,80 +16,94 @@ DIRECTIONS = {
 
 Ray = namedtuple("ray", "x y direction")
 
-rays = [Ray(0, 0, "right")]
 
-seen_rays = set()
+def process(rays=[Ray(0, 0, "right")]):
 
-while rays:
-    ray = rays.pop(0)
+    seen_rays = set()
 
-    char = grid.get((ray.y, ray.x))
+    while rays:
+        ray = rays.pop(0)
 
-    if not char:
-        continue
+        char = grid.get((ray.y, ray.x))
 
-    if (ray.x, ray.y, ray.direction) in seen_rays:
-        continue
+        if not char:
+            continue
 
-    seen_rays.add((ray.x, ray.y, ray.direction))
+        if (ray.x, ray.y, ray.direction) in seen_rays:
+            continue
 
-    match grid[(ray.y, ray.x)]:
-        case ".":
-            y = ray.y + DIRECTIONS[ray.direction][0]
-            x = ray.x + DIRECTIONS[ray.direction][1]
+        seen_rays.add((ray.x, ray.y, ray.direction))
 
-            rays.append(Ray(x, y, ray.direction))
-        case "/":
-            match ray.direction:
-                case "up":
-                    x = ray.x + DIRECTIONS["right"][1]
-                    rays.append(Ray(x, ray.y, "right"))
-                case "down":
-                    x = ray.x + DIRECTIONS["left"][1]
-                    rays.append(Ray(x, ray.y, "left"))
-                case "left":
-                    y = ray.y + DIRECTIONS["down"][0]
-                    rays.append(Ray(ray.x, y, "down"))
-                case "right":
-                    y = ray.y + DIRECTIONS["up"][0]
-                    rays.append(Ray(ray.x, y, "up"))
-        case "\\":
-            match ray.direction:
-                case "up":
-                    x = ray.x + DIRECTIONS["left"][1]
-                    rays.append(Ray(x, ray.y, "left"))
-                case "down":
-                    x = ray.x + DIRECTIONS["right"][1]
-                    rays.append(Ray(x, ray.y, "right"))
-                case "left":
-                    y = ray.y + DIRECTIONS["up"][0]
-                    rays.append(Ray(ray.x, y, "up"))
-                case "right":
-                    y = ray.y + DIRECTIONS["down"][0]
-                    rays.append(Ray(ray.x, y, "down"))
-        case "|":
-            match ray.direction:
-                case "up" | "down":
-                    y = ray.y + DIRECTIONS[ray.direction][0]
-                    rays.append(Ray(ray.x, y, ray.direction))
-                case "left" | "right":
-                    up_y = ray.y + DIRECTIONS["up"][0]
-                    down_y = ray.y + DIRECTIONS["down"][0]
+        match grid[(ray.y, ray.x)]:
+            case ".":
+                y = ray.y + DIRECTIONS[ray.direction][0]
+                x = ray.x + DIRECTIONS[ray.direction][1]
 
-                    rays.append(Ray(ray.x, up_y, "up"))
-                    rays.append(Ray(ray.x, down_y, "down"))
-        case "-":
-            match ray.direction:
-                case "up" | "down":
-                    left_x = ray.x + DIRECTIONS["left"][1]
-                    right_x = ray.x + DIRECTIONS["right"][1]
+                rays.append(Ray(x, y, ray.direction))
+            case "/":
+                match ray.direction:
+                    case "up":
+                        x = ray.x + DIRECTIONS["right"][1]
+                        rays.append(Ray(x, ray.y, "right"))
+                    case "down":
+                        x = ray.x + DIRECTIONS["left"][1]
+                        rays.append(Ray(x, ray.y, "left"))
+                    case "left":
+                        y = ray.y + DIRECTIONS["down"][0]
+                        rays.append(Ray(ray.x, y, "down"))
+                    case "right":
+                        y = ray.y + DIRECTIONS["up"][0]
+                        rays.append(Ray(ray.x, y, "up"))
+            case "\\":
+                match ray.direction:
+                    case "up":
+                        x = ray.x + DIRECTIONS["left"][1]
+                        rays.append(Ray(x, ray.y, "left"))
+                    case "down":
+                        x = ray.x + DIRECTIONS["right"][1]
+                        rays.append(Ray(x, ray.y, "right"))
+                    case "left":
+                        y = ray.y + DIRECTIONS["up"][0]
+                        rays.append(Ray(ray.x, y, "up"))
+                    case "right":
+                        y = ray.y + DIRECTIONS["down"][0]
+                        rays.append(Ray(ray.x, y, "down"))
+            case "|":
+                match ray.direction:
+                    case "up" | "down":
+                        y = ray.y + DIRECTIONS[ray.direction][0]
+                        rays.append(Ray(ray.x, y, ray.direction))
+                    case "left" | "right":
+                        up_y = ray.y + DIRECTIONS["up"][0]
+                        down_y = ray.y + DIRECTIONS["down"][0]
 
-                    rays.append(Ray(left_x, ray.y, "left"))
-                    rays.append(Ray(right_x, ray.y, "right"))
-                case "left" | "right":
-                    x = ray.x + DIRECTIONS[ray.direction][1]
-                    rays.append(Ray(x, ray.y, ray.direction))
+                        rays.append(Ray(ray.x, up_y, "up"))
+                        rays.append(Ray(ray.x, down_y, "down"))
+            case "-":
+                match ray.direction:
+                    case "up" | "down":
+                        left_x = ray.x + DIRECTIONS["left"][1]
+                        right_x = ray.x + DIRECTIONS["right"][1]
 
-seen_points = {(y, x) for (y, x, _) in seen_rays}
+                        rays.append(Ray(left_x, ray.y, "left"))
+                        rays.append(Ray(right_x, ray.y, "right"))
+                    case "left" | "right":
+                        x = ray.x + DIRECTIONS[ray.direction][1]
+                        rays.append(Ray(x, ray.y, ray.direction))
 
-print(len(seen_points))
+    energised_points = {(y, x) for (y, x, _) in seen_rays}
+
+    return len(energised_points)
+
+
+part_1 = process()
+print(f"{part_1=}")
+
+part_2 = max(
+    *[process([Ray(x, 0, "down")]) for x in range(len(inp[0]))],
+    *[process([Ray(x, len(inp)-1, "up")]) for x in range(len(inp[0]))],
+    *[process([Ray(0, y, "down")]) for y in range(len(inp))],
+    *[process([Ray(len(inp[0])-1, y, "down")]) for y in range(len(inp))]
+)
+
+print(f"{part_2=}")
