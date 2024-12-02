26bd4f9cefb0cb1e12b93535ab969b8d0e37a198
Ben Silverman <ben.silverman@starlingbank.com>
2024-12-02 10:27:46 +0000

    clean up day 1

--- 2024/01/01.ts
+++ 2024/01/01.ts
@@ -1,3 +1,4 @@
+import assert from "node:assert";
 import { readInput } from "../utils";
 
 const input = await readInput();
@@ -5,51 +6,38 @@ const input = await readInput();
 const start = performance.now();
 
 const sortedCols = input
-	.reduce<[number[], number[]]>(
-		(acc, line: string) => {
-			const [a, b] = line.split(/\s+/).map((val) => parseInt(val));
+  .reduce<[number[], number[]]>(
+    (acc, line: string) => {
+      const [a, b] = line.split("   ").map((val) => parseInt(val));
 
-			if (!a || !b) {
-				throw new Error(
-					"Could not split the line as expected. Check input file?"
-				);
-			}
+      assert(a && b);
 
-			acc[0].push(a);
-			acc[1].push(b);
+      acc[0].push(a);
+      acc[1].push(b);
 
-			return acc;
-		},
-		[[], []]
-	)
-	.map((arr) => arr.sort((a, b) => a - b)) as [number[], number[]];
+      return acc;
+    },
+    [[], []]
+  )
+  .map((arr) => arr.sort((a, b) => a - b)) as [number[], number[]];
 
-let part1 = 0;
-
-for (let i = 0; i < sortedCols[0].length; i++) {
-	const a = sortedCols[0][i];
-	const b = sortedCols[1][i];
-
-	if (!a || !b) {
-		throw new Error("Could not access one of the indexes in sortedCols.");
-	}
-
-	part1 += Math.abs(a - b);
-}
+const part1 = sortedCols[0].reduce(
+  (acc, val, i) => acc + Math.abs(val - sortedCols[1][i]!),
+  0
+);
 
 console.log("Part 1:", part1);
 
-const freqs = sortedCols[1]?.reduce((acc, val) => {
-	acc.set(val, (acc.get(val) ?? 0) + 1);
+const freqs = sortedCols[1].reduce((acc, val) => {
+  acc.set(val, (acc.get(val) ?? 0) + 1);
 
-	return acc;
+  return acc;
 }, new Map<number, number>());
 
-const part2 = sortedCols[0]!.reduce((acc, val) => {
-	acc += val * (freqs.get(val) ?? 0);
-
-	return acc;
-}, 0);
+const part2 = sortedCols[0].reduce(
+  (acc, val) => acc + val * (freqs.get(val) ?? 0),
+  0
+);
 
 const end = performance.now();
 const ms = (end - start).toFixed(3);
