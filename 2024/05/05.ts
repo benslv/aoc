ad6102c3ef53075f08051c1553044f0aaf311d76
Ben Silverman <ben.silverman@starlingbank.com>
2024-12-05 13:07:48 +0000

    DFS topsort

--- 2024/05/05.ts
+++ 2024/05/05.ts
@@ -6,10 +6,10 @@ const [rules, updates] = await readInput().then((lines) => {
   return [lines.slice(0, sepIndex), lines.slice(sepIndex + 1)];
 });
 
-const orderMap = new Map<string, Set<string>>();
+const orderMap = new Map<number, Set<number>>();
 
 for (const rule of rules) {
-  const [before, after] = rule.split("|");
+  const [before, after] = rule.split("|").map(Number);
 
   if (!orderMap.has(before)) {
     orderMap.set(before, new Set());
@@ -18,7 +18,7 @@ for (const rule of rules) {
   orderMap.get(before)?.add(after);
 }
 
-function processUpdate(update: string[]) {
+function checkUpdate(update: number[]) {
   const seen = new Set();
   for (const page of update) {
     const succ = orderMap.get(page) ?? new Set();
@@ -32,16 +32,46 @@ function processUpdate(update: string[]) {
 }
 
 let part1 = 0;
+let part2 = 0;
 
 for (const update of updates) {
-  const splitUpdate = update.split(",");
-  if (processUpdate(splitUpdate)) {
-    console.log(splitUpdate);
+  const splitUpdate = update.split(",").map(Number);
+  if (checkUpdate(splitUpdate)) {
+    const midpoint = Math.floor(splitUpdate.length / 2);
 
-    console.log(splitUpdate[Math.round(splitUpdate.length / 2) - 1]);
+    part1 += splitUpdate[midpoint];
+  } else {
+    const update = dfsTopologicalSort(splitUpdate);
+    const midpoint = Math.floor(update.length / 2);
 
-    part1 += parseInt(splitUpdate[Math.round(splitUpdate.length / 2) - 1]);
+    part2 += update[midpoint];
   }
 }
 
 console.log(part1);
+console.log(part2);
+
+function dfsTopologicalSort(update: number[]): number[] {
+  const visited = new Set();
+  const ordered: number[] = [];
+
+  for (const num of update) {
+    traverse(num);
+  }
+
+  function traverse(num: number) {
+    if (visited.has(num)) return;
+    visited.add(num);
+
+    const allDeps = orderMap.get(num) ?? new Set();
+    const depsActuallyInGraph = allDeps.intersection(new Set(update));
+
+    for (const dep of depsActuallyInGraph) {
+      traverse(dep);
+    }
+
+    ordered.unshift(num);
+  }
+
+  return ordered;
+}
