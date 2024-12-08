2105d2f2623675dd4db0c1e4900deeb1c94dcccc
Ben Silverman <bensilverman10@gmail.com>
2024-12-08 16:57:35 +0000

    base part 1 off part 2

--- 2024/08/08.ts
+++ 2024/08/08.ts
@@ -1,4 +1,4 @@
-import { readInput } from "../utils";
+import { readInput, time } from "../utils";
 
 const input = await readInput();
 
@@ -21,42 +21,34 @@ for (const a of antennae) {
 const partOneAntinodes = new Set();
 const partTwoAntinodes = new Set();
 
-for (const antenna in antennaMap) {
-	const positions = antennaMap[antenna];
+time(() => {
+	for (const antenna in antennaMap) {
+		const positions = antennaMap[antenna];
 
-	for (let i = 0; i < positions.length - 1; i++) {
-		for (let j = i + 1; j < positions.length; j++) {
-			const posA = positions[i];
-			const posB = positions[j];
+		for (let i = 0; i < positions.length - 1; i++) {
+			for (let j = i + 1; j < positions.length; j++) {
+				const posA = positions[i];
+				const posB = positions[j];
 
-			for (const [y, x] of getAntinodePositions(posA, posB)) {
-				if (y >= 0 && y < height && x >= 0 && x < width) {
-					partOneAntinodes.add(`${y},${x}`);
-				}
-			}
+				for (const [y, x] of getAllPointsOnLine(posA, posB)) {
+					if (y >= 0 && y < height && x >= 0 && x < width) {
+						if (
+							y - posA[0] === 2 * (y - posB[0]) ||
+							y - posB[0] === 2 * (y - posA[0])
+						) {
+							partOneAntinodes.add(`${y},${x}`);
+						}
 
-			for (const [y, x] of getAllPointsOnLine(posA, posB)) {
-				if (y >= 0 && y < height && x >= 0 && x < width) {
-					partTwoAntinodes.add(`${y},${x}`);
+						partTwoAntinodes.add(`${y},${x}`);
+					}
 				}
 			}
 		}
 	}
-}
 
-console.log("Part 1:", partOneAntinodes.size);
-console.log("Part 2:", partTwoAntinodes.size);
-
-function* getAntinodePositions(
-	[aY, aX]: [number, number],
-	[bY, bX]: [number, number]
-): Generator<[number, number]> {
-	const dy = bY - aY;
-	const dx = bX - aX;
-
-	yield [bY + dy, bX + dx];
-	yield [aY - dy, aX - dx];
-}
+	console.log("Part 1:", partOneAntinodes.size);
+	console.log("Part 2:", partTwoAntinodes.size);
+});
 
 function* getAllPointsOnLine(
 	[aY, aX]: [number, number],
