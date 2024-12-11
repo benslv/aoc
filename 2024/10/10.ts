7d568d73fdc8d3ebb531a67e2919c87b285f02f2
Ben Silverman <ben.silverman@starlingbank.com>
2024-12-11 10:43:50 +0000

    2024 Day 10

--- 2024/10/10.ts
+++ 2024/10/10.ts
@@ -4,20 +4,15 @@ const input = await readInput();
 
 const grid = input.map((line) => line.split("").map(Number));
 
-console.log(grid);
-
 function getAdjacentCoords(y: number, x: number): [number, number][] {
 	const coords: [number, number][] = [];
 
+	const currHeight = grid[y][x];
+
 	for (const dy of [-1, 0, 1]) {
 		for (const dx of [-1, 0, 1]) {
-			if (
-				Math.abs(dy) + Math.abs(dx) === 1 &&
-				y + dy >= 0 &&
-				y + dy < grid.length &&
-				x + dx >= 0 &&
-				x + dx < grid[0].length
-			) {
+			const newHeight = grid[y + dy]?.[x + dx];
+			if (Math.abs(dy) + Math.abs(dx) === 1 && newHeight === currHeight + 1) {
 				coords.push([y + dy, x + dx]);
 			}
 		}
@@ -27,27 +22,47 @@ function getAdjacentCoords(y: number, x: number): [number, number][] {
 }
 
 function DFS(grid: number[][]) {
+	const part1 = new Map<string, Set<string>>();
+	const part2: string[][] = [];
+
 	for (let y = 0; y < grid.length; y++) {
 		for (let x = 0; x < grid[y].length; x++) {
-			console.log("Starting at", y, x);
-
-			const paths = traverse(y, x);
-
-			console.log(paths);
+			if (grid[y][x] === 0) {
+				traverse(y, x);
+			}
 		}
 	}
 
 	function traverse(y: number, x: number, path: string[] = []) {
-		for (const [y2, x2] of getAdjacentCoords(y, x)) {
-			if (grid[y2][x2] === 9) {
-				return [...path, `${y2},${x2}`];
+		if (grid[y][x] === 9) {
+			part2.push([...path, `${y},${x}`]);
+
+			const start = path[0];
+
+			if (!part1.has(start)) {
+				part1.set(start, new Set());
 			}
 
-			if (!path.includes(`${y2},${x2}`)) {
-				return traverse(y2, x2, [...path, `${y2},${x2}`]);
-			}
+			part1.get(start)!.add(`${y},${x}`);
+
+			return;
+		}
+
+		for (const point of getAdjacentCoords(y, x)) {
+			if (path.includes(`${y},${x}`)) continue;
+
+			traverse(...point, [...path, `${y},${x}`]);
 		}
 	}
+
+	return [part1, part2] as const;
 }
 
-DFS(grid);
+const [part1, part2] = DFS(grid);
+
+console.log(
+	"Part 1:",
+	part1.entries().reduce((acc, [, ends]) => acc + ends.size, 0)
+);
+
+console.log("Part 2:", part2.length);
