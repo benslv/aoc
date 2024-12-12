8ffff8b0222eb01f3bb5ed51fc58d270c807aa2d
Ben Silverman <ben.silverman@starlingbank.com>
2024-12-12 11:22:43 +0000

    start Day 12

---
+++ 2024/12/12.ts
@@ -0,0 +1,94 @@
+import { readInput } from "../utils";
+
+const input = await readInput();
+
+function getRegions(grid: string[]): Array<Set<string>> {
+	const pointsAlreadyInARegion = new Set<string>();
+	const regions = [];
+
+	for (let y = 0; y < grid.length; y++) {
+		const line = grid[y];
+		for (let x = 0; x < line.length; x++) {
+			const region = floodFill(y, x);
+
+			if (!region) continue;
+
+			for (const point of region) {
+				pointsAlreadyInARegion.add(point);
+			}
+
+			regions.push(region);
+		}
+	}
+
+	return regions;
+
+	function floodFill(y: number, x: number): Set<string> | undefined {
+		const points = new Set<string>([`${y},${x}`]);
+		const initialValue = input[y][x];
+
+		const queue: [number, number][] = [[y, x]];
+
+		while (queue.length > 0) {
+			const [y, x] = queue.shift()!;
+
+			if (pointsAlreadyInARegion.has(`${y},${x}`)) {
+				return undefined;
+			}
+
+			for (const dy of [-1, 0, 1]) {
+				for (const dx of [-1, 0, 1]) {
+					const nextY = y + dy;
+					const nextX = x + dx;
+
+					if (points.has(`${nextY},${nextX}`)) continue;
+
+					if (dy !== dx && input[nextY]?.[nextX] === initialValue) {
+						queue.push([nextY, nextX]);
+						points.add(`${nextY},${nextX}`);
+					}
+				}
+			}
+		}
+
+		return points;
+	}
+}
+
+const regions = getRegions(input);
+
+function getExposedSides(y: number, x: number, grid: string[]): number {
+	const value = grid[y]?.[x];
+
+	const top = grid[y - 1]?.[x];
+	const bottom = grid[y + 1]?.[x];
+	const left = grid[y]?.[x - 1];
+	const right = grid[y]?.[x + 1];
+
+	return [top, bottom, left, right].reduce(
+		(acc, num) => (num !== value ? acc + 1 : acc),
+		0
+	);
+}
+
+function getPerimeter(region: Set<string>, grid: string[]): number {
+	return region.values().reduce((acc, point) => {
+		const [y, x] = point.split(",").map(Number);
+
+		return acc + getExposedSides(y, x, grid);
+	}, 0);
+}
+
+const part1 = () => {
+	const regions = getRegions(input);
+
+	const result = regions.reduce((acc, region) => {
+		const perimeter = getPerimeter(region, input);
+
+		return acc + region.size * perimeter;
+	}, 0);
+
+	return result;
+};
+
+console.log("Part 1:", part1());
