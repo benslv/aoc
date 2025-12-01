f3eb38090b47dc38744f4255bd61fd36619f2bf1
Ben Silverman <ben.silverman@starlingbank.com>
2025-12-01 10:23:02 +0000

    parse before running

--- 2025/01/01.ts
+++ 2025/01/01.ts
@@ -2,17 +2,15 @@ import { bench, run } from "mitata";
 import { readInput } from "../utils";
 import { mod } from "../utils/mod";
 
-const input = await readInput();
+const ROTATON_REGEX = /([LR])(\d+)/g
 
-const ROTATON_REGEX = /([LR])(\d+)/
+const input = await readInput().then(data => Array.from(data.join("\n").matchAll(ROTATON_REGEX)));
 
 function partOne() {
 	let rot = 50;
 	let answer = 0;
 
-	for (const line of input) {
-		const [, direction, amount] = line.match(ROTATON_REGEX);
-
+	for (const [, direction, amount] of input) {
 		switch (direction) {
 			case "L": {
 				rot = mod((rot - Number(amount)), 100);
@@ -33,9 +31,7 @@ function partTwo() {
 	let rot = 50;
 	let answer = 0;
 
-	for (const line of input) {
-		const [, direction, amount] = line.match(ROTATON_REGEX);
-
+	for (const [, direction, amount] of input) {
 		switch (direction) {
 			case "L": {
 				for (let i = 0; i < Number(amount); i++) {
@@ -58,10 +54,6 @@ function partTwo() {
 
 	return answer
 }
-
-
-
-
 
 console.log("Part 1:", partOne());
 console.log("Part 2:", partTwo());
