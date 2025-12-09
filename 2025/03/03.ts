c815ed476b1a39b418d09481488db4d9d6e5b3f0
Ben Silverman <bensilverman10@gmail.com>
2025-12-09 16:32:08 +0000

    Improve and format Day 3

--- 2025/03/03.ts
+++ 2025/03/03.ts
@@ -1,34 +1,44 @@
 import { bench, run } from "mitata";
 import { readInput } from "../utils";
 
-const input = await readInput().then(data => data.map(line => line.split("").map((n => parseInt(n)))));
+const input = await readInput().then((data) =>
+	data.map((line) => line.split("").map(Number))
+);
 
 function partOne() {
-	return input.reduce((total, bank) => total + findLargestJoltageForNBatteries(bank, 2), 0);
+	return input.reduce(
+		(total, bank) => total + findLargestJoltageForNBatteries(bank, 2),
+		0
+	);
 }
 
 function partTwo() {
-	return input.reduce((total, bank) => total + findLargestJoltageForNBatteries(bank, 12), 0);
+	return input.reduce(
+		(total, bank) => total + findLargestJoltageForNBatteries(bank, 12),
+		0
+	);
 }
 
-function findLargestJoltageForNBatteries(bank: Array<number>, n: number): number {
-	const array = Array.from({ length: n }, () => 0)
+function findLargestJoltageForNBatteries(
+	bank: Array<number>,
+	n: number
+): number {
+	const array: number[] = [];
 
-	for (let i = 0; i < array.length; i++) {
-		const prevIndex = i === 0 ? 0 : array[i - 1] + 1
+	for (let i = 0; i < n; i++) {
+		const prevIndex = i === 0 ? 0 : array[i - 1] + 1;
 
-		array[i] = maxIndex(bank, prevIndex, bank.length - (n - (i + 1)))
+		array.push(maxIndex(bank, prevIndex, bank.length - (n - (i + 1))));
 	}
 
-	return array.reduce((acc, val, i) => acc + bank[val] * Math.pow(10, array.length - (i + 1)), 0)
+	return array.reduce((acc, val) => 10 * acc + bank[val], 0);
 }
 
-
 function maxIndex(array: Array<number>, start: number, end: number): number {
-	let m = Math.max(start, 0)
+	let m = start;
 
 	for (let i = start; i < end; i++) {
-		if (array[i] > array[m]) m = i
+		if (array[i] > array[m]) m = i;
 	}
 
 	return m;
@@ -37,6 +47,20 @@ function maxIndex(array: Array<number>, start: number, end: number): number {
 console.log("Part 1:", partOne());
 console.log("Part 2:", partTwo());
 
-bench("Part 1", partOne)
-bench("Part 2", partTwo)
-run()
+bench("Part 1", partOne);
+bench("Part 2", partTwo);
+run();
+
+// lineplot(() => {
+// 	summary(() => {
+// 		bench("$num", function* (state: any) {
+// 			const num = state.get("num");
+// 			yield () =>
+// 				input.reduce(
+// 					(total, bank) =>
+// 						total + findLargestJoltageForNBatteries(bank, num),
+// 					0n
+// 				);
+// 		}).range("num", 1, 100);
+// 	});
+// });
