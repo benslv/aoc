287d641de331c9467324752365ee050a8f8d0d5b
Ben Silverman <bensilverman10@gmail.com>
2025-12-09 08:59:25 +0000

    Day 8

--- 2025/08/08.ts
+++ 2025/08/08.ts
@@ -2,33 +2,44 @@ import { readInput } from "../utils";
 
 const input = await readInput();
 
-const circuits = new Map<string, Set<string>>(input.map(line => [line, new Set([line])]));
-
-console.log(circuits);
+const circuits = new Set<Set<string>>(input.map((line) => new Set([line])));
 
 function calculate3dDistance(a: string, b: string): number {
-	const [x, y, z] = a.split(",").map(Number)
-	const [i, j, k] = b.split(",").map(Number)
+	const [x, y, z] = a.split(",").map(Number);
+	const [i, j, k] = b.split(",").map(Number);
 
-	const dist = Math.sqrt(Math.pow(x - i, 2) + Math.pow(y - j, 2) + Math.pow(z - k, 2))
+	const dist = Math.sqrt(
+		Math.pow(x - i, 2) + Math.pow(y - j, 2) + Math.pow(z - k, 2)
+	);
 
-	return dist
+	return dist;
 }
 
-const pairs = []
+const pairs = [];
 for (let i = 0; i < input.length - 1; i++) {
 	for (let j = i + 1; j < input.length; j++) {
-		pairs.push([input[i], input[j]] as const)
+		pairs.push([input[i], input[j]] as const);
 	}
 }
 
-pairs.sort((a, b) => calculate3dDistance(...a) - calculate3dDistance(...b))
+pairs.sort((a, b) => calculate3dDistance(...a) - calculate3dDistance(...b));
 
-
-const NUM_PAIRS_TO_CONNECT = 10;
-console.log(pairs.slice(0, NUM_PAIRS_TO_CONNECT));
+const NUM_PAIRS_TO_CONNECT = 1000;
 
 for (const [i, [a, b]] of pairs.entries()) {
+	if (i === NUM_PAIRS_TO_CONNECT) {
+		const sortedCircuits = Array.from(circuits).toSorted(
+			(a, b) => b.size - a.size
+		);
+
+		const partOne =
+			sortedCircuits[0].size *
+			sortedCircuits[1].size *
+			sortedCircuits[2].size;
+
+		console.log("Part 1:", partOne);
+	}
+
 	let aCircuit, bCircuit;
 
 	for (const c of circuits) {
@@ -36,37 +47,16 @@ for (const [i, [a, b]] of pairs.entries()) {
 		if (c.has(b)) bCircuit = c;
 	}
 
-	circuits.delete(aCircuit!)
-	circuits.delete(bCircuit!)
+	circuits.delete(aCircuit!);
+	circuits.delete(bCircuit!);
 
-	circuits.add(aCircuit!.union(bCircuit!))
+	circuits.add(aCircuit!.union(bCircuit!));
 
-	if (i === 10) break;
+	if (circuits.size === 1) {
+		const [x1] = a.split(",").map(Number);
+		const [x2] = b.split(",").map(Number);
+
+		console.log("Part 2:", x1 * x2);
+		break;
+	}
 }
-
-console.log(circuits);
-
-
-// console.log(circuitMap);
-
-
-// console.log(getDisjointCircuits(circuitMap).toSorted((a, b) => b.size - a.size));
-
-
-// function getDisjointCircuits(map: Map<string, Set<string>>): Array<Set<string>> {
-// 	const seen: Set<string> = new Set();
-
-// 	const circuits = [];
-
-// 	for (const [point, circuit] of map.entries()) {
-// 		if (seen.has(point)) continue;
-
-// 		for (const p of circuit) {
-// 			seen.add(p)
-// 		}
-
-// 		circuits.push(circuit)
-// 	}
-
-// 	return circuits;
-// }
