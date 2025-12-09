f12de3db243c592a3e2a1c10fb11943894599047
Ben Silverman <bensilverman10@gmail.com>
2025-12-09 15:12:40 +0000

    Format Day 2

--- 2025/02/02.ts
+++ 2025/02/02.ts
@@ -1,12 +1,15 @@
 import { bench, run } from "mitata";
 import { readInput } from "../utils";
 
-const P1_REGEX = /^(\d+)\1$/
-const P2_REGEX = /^(\d+)(\1)+$/
+const P1_REGEX = /^(\d+)\1$/;
+const P2_REGEX = /^(\d+)\1+$/;
 
 const input = await readInput();
 
-const ranges = input.at(0)!.split(",").map(r => r.split("-").map(Number))
+const ranges = input
+	.at(0)!
+	.split(",")
+	.map((r) => r.split("-").map(Number));
 
 function solve() {
 	let partOne = 0;
@@ -25,6 +28,5 @@ function solve() {
 const [part1, part2] = solve();
 console.log(`Part 1: ${part1}\nPart 2: ${part2}`);
 
-
-bench("Part 1 & 2", solve)
-run()
+bench("Part 1 & 2", solve);
+run();
